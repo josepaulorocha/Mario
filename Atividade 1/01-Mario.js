88d0649aeefb8a7b167444f983f2937c030cb9e1
José Paulo De Sá Rocha <josepaulo.sa.rocha@gmail.com>
2025-05-19 10:34:10 -0300

    Update 01-Mario.js

--- Atividade 1/01-Mario.js
+++ Atividade 1/01-Mario.js
@@ -1,1 +1,10 @@
+// @ts-check
+/* eslint-disable */
 
+function output(t) {
+    document.write('<p>' + t + '</p>');
+  }
+  
+  alert('Mario game');
+  console.log('Mario game');
+  output('Mario World');
